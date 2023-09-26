8add8985ff52c8bfe8d1be3ede2d8da1b8f78899
Otto Pekander <otto.pekander@gmail.com>
2023-09-26 17:45:52 +0300

    end of step1

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Header render course name
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
 
+const Content = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    {props.exercises.map((item, index) => (
+      <p key={index}>
+        {item.name} {item.exercises}
       </p>
+      ))}
     </>
+  );
+}
+
+
+// Total render total number of exercises
+const Total = (props) => {
+  // calculate the number of exercises from array of objects
+  let total = 0;
+
+  props.exercises.forEach(item => {
+    total += item.exercises
+  });
+
+  return(
+    <p>Number of exercises {total}</p>
+  );
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using props to pass data', exercises: 7},
+    {name: 'State of a component', exercises: 14}
+  ]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content exercises={exercises}/>
+      <Total exercises={exercises}/>
+    </div>
   )
 }
 
