a407a5d54d11a8a2059814ea095760fc03081b18
Otto Pekander <otto.pekander@gmail.com>
2023-09-28 18:23:37 +0300

    deleted remains of old code which was not used in prvious step

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -40,11 +40,6 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const exercises = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14}
-  ]
 
   const parts = [
     {
